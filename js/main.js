e4e8a786921c04ee1d40add288580257450c6b9f
Domisalej6921 <dominickgeorge21@gmail.com>
2024-10-21 10:32:57 +0100

    Altered project directory layout

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const responsiveMenuButton = document.getElementById('responsiveMenuToggleButton');
-
-const navBarLinks =document.querySelector('.menu');
-
-responsiveMenuButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('open');
-    responsiveMenuButton.classList.toggle('open');
-});
